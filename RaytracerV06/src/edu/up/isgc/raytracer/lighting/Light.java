3ca729eb4127bd06aa73ca187bef2c8b5bd428ef
Rodrigo López <rodlopg@gmail.com>
2025-05-05 18:55:17 -0600

    Starting to add the phong SHADING and using ericson for barycentric

--- RaytracerV06/src/edu/up/isgc/raytracer/lighting/Light.java
+++ RaytracerV06/src/edu/up/isgc/raytracer/lighting/Light.java
@@ -67,5 +67,28 @@ public abstract class Light {
         return new Color(nLOC[0] * lambertian, nLOC[1] * lambertian, nLOC[2] * lambertian);
     }
 
+    public static Vector3D ericson(Vector3D point, Vector3D v1, Vector3D v2, Vector3D v3) {
+        Vector3D e1 = Vector3D.subtract(v2,v1);
+        Vector3D e2 = Vector3D.subtract(v3,v1);
+        Vector3D eP = Vector3D.subtract(point,v1);
+
+        Double d11 = e1.dot(e1);
+        Double d12 = e1.dot(e2);
+        Double d22 = e2.dot(e2);
+        Double d1P = e1.dot(eP);
+        Double d2P = e2.dot(eP);
+
+        Double denominator = (d11*d22)-(d12*d12);
+        Double v = ( (d22*d1P)-(d12*d2P) )/denominator;
+        Double w = ( (d11*d2P)-(d12*d1P) )/denominator;
+        Double u = 1-v-w;
+
+        Vector3D vV = v1.scale(v);
+        Vector3D wV = v2.scale(w);
+        Vector3D uV = v3.scale(u);
+
+        return vV.add(wV).add(uV);
+    }
+
     public static ArrayList<Light> getLights(){return Light.lights;}
 }
