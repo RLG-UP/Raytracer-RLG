f5a913b81415f81be2e3d6924f6be8a648cf6727
Rodrigo López <rodlopg@gmail.com>
2025-05-14 14:22:00 -0600

    Making a commit after solving merge conflicts when reverting

--- RaytracerV06/src/edu/up/isgc/raytracer/shapes/models/Polygon.java
+++ RaytracerV06/src/edu/up/isgc/raytracer/shapes/models/Polygon.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 public class Polygon {
     private ArrayList<Triangle> shape;
     private Color color;
+    private Vector3D position = Vector3D.getZero();
 
     public Polygon(ArrayList<Triangle> shape){
         this.setShape(shape);
@@ -105,7 +106,7 @@ public class Polygon {
     public void setColor(Color color) {
         this.color = color;
     }
-<<<<<<< HEAD
+    
 
     public void translate(float tX, float tY, float tZ){
         for(Triangle t : shape){
@@ -190,7 +191,5 @@ public class Polygon {
     public void setPosition(Vector3D position) {
         this.position = position;
     }
-=======
->>>>>>> parent of b5cf2c9 (Just added more things to translate polygons, I think it is ready to test (added more logic to correctly translate complete objects))
 }
 
