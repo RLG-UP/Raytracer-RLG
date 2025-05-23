da0d655f1e6749e36e9a7ca4ff21b3a20cbd6184
Rodrigo López <rodlopg@gmail.com>
2025-05-23 11:50:08 -0600

    Sending saved rage scene components

--- RaytracerV08/src/edu/up/isgc/raytracer/Raytracer.java
+++ RaytracerV08/src/edu/up/isgc/raytracer/Raytracer.java
@@ -89,7 +89,8 @@ public class Raytracer {
         //scene.addPolygon( polygon );
         //scene.addPolygon( polygon2 );
 
-        //SCREAM SCENE
+        //RAGE SCENE
+        /*
         Scene scene = new Scene(new Color(220, 20, 60));
         String objPath = new File("W:\\-UP_PC-\\4th_SEMESTER\\MMCG_FOURTH_SEMESTER_RLG\\RayTracer_Objs\\Scream_Scene\\RenderOBJS\\Rage_Shards.obj").getAbsolutePath();
         String objPath01 = new File("W:\\-UP_PC-\\4th_SEMESTER\\MMCG_FOURTH_SEMESTER_RLG\\RayTracer_Objs\\Scream_Scene\\RenderOBJS\\Rage_Shards2.obj").getAbsolutePath();
@@ -99,22 +100,25 @@ public class Raytracer {
         Obj.RenderObj(scene, objPath01, Material.GLASS(Color.BLACK));
         Obj.RenderObj(scene, objPath02, Material.GLASS(Color.BLACK));
         Obj.RenderObj(scene, objPath03, Material.GLASS(Color.WHITE));
-        //scene.addObject(new Sphere(new Vector3D(0, 0, 0), 2, Material.GLASS(Color.WHITE)));
+        Camera camera = new Camera(new Vector3D(-0.005, -0.005, -5.5), nearPlane, farPlane, 30, width, height);
+
+        //RAGE Lights
+        Light light01 = new Point(5f, Color.white, new Vector3D(0, 1.1, -7));
+        Light light02 = new Point(5f, Color.white, new Vector3D(0, -1.6593, -7));
+        Light light04 = new Spot(1f, Color.white, new Vector3D(0,0,-10), new Vector3D(4, 0, 0), 1f, 1f);
+        Light light05 = new Spot(1f, Color.white, new Vector3D(0,0,-10), new Vector3D(-4, 0, 0), 1f, 1f);
+         */
 
         // Set up camera at the origin
         //Camera camera = new Camera(new Vector3D(-0.008, -0.005, -1.5), nearPlane, farPlane, 60, width, height);
         //Camera camera = new Camera(new Vector3D(-0.005, -0.005, -4.75), nearPlane, farPlane, 30, width, height);
-        Camera camera = new Camera(new Vector3D(-0.005, -0.005, -5.5), nearPlane, farPlane, 30, width, height);
-        //ScreamLights
-        Light light01 = new Point(5f, Color.white, new Vector3D(0, 1.1, -7));
-        Light light02 = new Point(5f, Color.white, new Vector3D(0, -1.6593, -7));
 
         //Light light0D = new Directional(1, Color.white, new Vector3D(0,0,10), new Vector3D(2, 0, 0));
         //Light light02 = new Directional(100, Color.blue, new Vector3D(0,-10,0), new Vector3D(0,0,0));
         //Light light03 = new Point(10f, Color.white, new Vector3D(0, -1, -2.5));
-        Light light04 = new Spot(1f, Color.white, new Vector3D(0,0,-10), new Vector3D(4, 0, 0), 1f, 1f);
-        Light light05 = new Spot(1f, Color.white, new Vector3D(0,0,-10), new Vector3D(-4, 0, 0), 1f, 1f);
 
+        Scene scene = new Scene(new Color(220, 20, 60));
+        Camera camera = new Camera(new Vector3D(-0.005, -0.005, -5.5), nearPlane, farPlane, 30, width, height);
 
         Renderer.renderScene(width, height, camera, scene);
 
